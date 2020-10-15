28ea49944586f40390d28716e9f514b782f6a3a9
steven huynh <steven.h@live.com>
2020-10-14 21:31:09 -0700

    added button to allow grid to be resized & clear, also grid takes up same amount of space independent of amount of cells.

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const GRID_SIZE = 16;
 const grid = document.querySelector('.grid');
 
-for (let i = 1; i <= GRID_SIZE; ++i) {
-    for (let j = 1; j <= GRID_SIZE; ++j) {
-        const div = document.createElement('div');
-        div.setAttribute('id', `${i},${j}`);
-        div.classList.add('cell');
-        grid.appendChild(div);
+let createGrid = (GRID_SIZE = 16) => {
+    for (let i = 1; i <= GRID_SIZE; ++i) {
+        for (let j = 1; j <= GRID_SIZE; ++j) {
+            const div = document.createElement('div');
+            div.setAttribute('id', `${i},${j}`);
+            div.classList.add('cell');
+            grid.appendChild(div);
+        }
     }
-}
 
-let cells = document.querySelectorAll('.cell');
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', e => {
-        e.target.style.cssText = 'background-color: rgb(98, 98, 98);'
+    grid.style.setProperty('--grid-size', `${GRID_SIZE}`);
+
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', e => {
+            e.target.style.cssText = 'background-color: rgb(98, 98, 98);'
+        });
     });
+};
+
+
+const clearGrid = document.querySelector('#clear-grid');
+clearGrid.addEventListener('click', () => {
+    while (true) {
+        gSize = prompt('enter a number between 1 and 100');
+        if ((gSize >= 1 && gSize <= 100) || gSize === null) { break; }
+    }
+
+    if (gSize) { removeExistingGrid(); createGrid(gSize); }
 });
+
+
+let removeExistingGrid = () => {
+    let divs = document.querySelectorAll('.cell');
+    divs.forEach(div => grid.removeChild(div));
+};
+
+createGrid();
